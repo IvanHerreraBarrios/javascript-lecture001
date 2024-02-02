5f22c5611d9d007ffa1f05ee1ceca4ed6126af75
Ivan Herrera Barrios <ivanherrerabarrios16@gmail.com>
2024-02-01 18:40:06 -0600

    Introduction

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("Hello World from JS");
+console.log("And he said: 'Hello World'")
+console.log("And he said: \"Hello World\"")
+
+let age = 5+4;
+let thisIsAVariable = true;
+
+age = 10;
+
+console.log("Age:" + age);
+console.log(thisIsAVariable);
+
+let pet;
+console.log("Pet: " + pet); //It is not defined
+
+const name = prompt("Please write your name");
